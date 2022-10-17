c0b1f914859c8023a0778d13b9b10e6301277833
Marcus <108342348+iTech20@users.noreply.github.com>
2022-10-17 10:23:23 -0300

    v1.0

---
+++ ProjetoCalculos/src/projetocalculos/ProjetoCalculos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projetocalculos;
+
+/**
+ *
+ * @author 42labinfo
+ */
+public class ProjetoCalculos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
